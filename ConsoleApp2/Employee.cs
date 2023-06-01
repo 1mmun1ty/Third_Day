acf6591b3f592958ddf32979da3459c801fbbc26
1mmun1ty <100227332+1mmun1ty@users.noreply.github.com>
2023-06-01 12:02:51 +0300

    EmployeeBooks

--- ConsoleApp2/Employee.cs
+++ ConsoleApp2/Employee.cs
@@ -1,43 +1,45 @@
-﻿
-public class Employee
+﻿namespace Employee
 {
-	private string _fio;
-	private int _department;
-	private double _salary;
-	public int _id;
-	static public int _count = 1;
+	public class Employee
+	{
+		private string _fio;
+		private int _department;
+		private double _salary;
+		public int _id;
+		static public int _count = 1;
 
-	public Employee(string fio, int department, double salary)
-	{
-		_department = department;
-		_salary = salary;
-		_fio = fio;
-		_id = _count++;
-	}
-	public string Fio
-	{
-		get => _fio;
-		set => _fio = value;
-	}
-	public int Department
-	{
-		get => _department;
-		set => _department = value;
-	}
-	public double Salary
-	{
-		get => _salary;
-		set => _salary = value;
-	}
-
-	public double FindMinSalaryInDepartment(List<Employee> employees, int departmentId)
-	{
-		var departmentEmployees = employees.Where(e => e.Department == departmentId);
-		if (departmentEmployees.Any())
+		public Employee(string fio, int department, double salary)
 		{
-			return departmentEmployees.Min(e => e.Salary);
+			_department = department;
+			_salary = salary;
+			_fio = fio;
+			_id = _count++;
 		}
-		return 0;
+		public string Fio
+		{
+			get => _fio;
+			set => _fio = value;
+		}
+		public int Department
+		{
+			get => _department;
+			set => _department = value;
+		}
+		public double Salary
+		{
+			get => _salary;
+			set => _salary = value;
+		}
+
+		public double FindMinSalaryInDepartment(List<Employee> employees, int departmentId)
+		{
+			var departmentEmployees = employees.Where(e => e.Department == departmentId);
+			if (departmentEmployees.Any())
+			{
+				return departmentEmployees.Min(e => e.Salary);
+			}
+			return 0;
+		}
 	}
 }
 
